5c094ebbe8d2107b685e7b1d6b0bc34ad4e02340
samuel <samupoke1@gmail.com>
2025-03-14 17:04:58 -0300

    1a criando tabuleiro

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ var vencedor=' ';
 var fimJogo=false;
 var tab=[3];
 
+
+for(i=0;i<3;i++){
+tab[i]=[3];
+    for(j=0;j<3;j++){
+        tab[i][j]=' ';
+    }
+}
+
 while ((jogadas<9)&&(fimJogo==false)){
     lin= parseInt(prompt(`${jogador} em [linha]: `));
     col=parseInt(prompt(`${jogador} em [coluna]: `));
